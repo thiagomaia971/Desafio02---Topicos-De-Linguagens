53ee1ce5978739d7166a947c963d6c7049e95722
thiagomcoffice <thiago.pivotwow@gmail.com>
2015-08-31 19:00:21 -0300

    Finish

--- NaveDesafio/src/Classes/MainClass.java
+++ NaveDesafio/src/Classes/MainClass.java
@@ -2,53 +2,68 @@ package Classes;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		long timeIni = System.currentTimeMillis(), timeFinal, deltaTime;
+		//CriarTxt("arquivo.txt");
+		float[] resultadoDistancia = new float[3];
 
-		float[] avioesDistancia = new float[3];
+		float[] coordenadasX = new float[NumeroAvioes("arquivo.txt")+1];
+		float[] coordenadasY = new float[coordenadasX.length];
+
+		PopularArray("arquivo.txt", coordenadasX, coordenadasY);
+
+		AvioesDistancia("arquivo.txt", resultadoDistancia, coordenadasX,
+				coordenadasY);
+
+		timeFinal = System.currentTimeMillis();
 		
-		float[] coordenadas = new float[NumeroAvioes("arquivo.txt")];
+		deltaTime = timeFinal - timeIni;
 		
-		AvioesDistancia("arquivo.txt", avioesDistancia);
-
-		System.out.printf("O avião %.0f com o avião %.0f tem a distancia: %.7f", 
-				avioesDistancia[0], avioesDistancia[1], avioesDistancia[2]);
-
+		System.out.printf(
+				"O avião %.0f com o avião %.0f tem a distancia: %.2f\nTime: %d (ms)",
+				resultadoDistancia[0], resultadoDistancia[1],
+				resultadoDistancia[2], deltaTime);
 	}
+	
+	private static float[] AvioesDistancia(String File,
+			float[] avioesDistancia, float[] coordenadasX, float[] coordenadasY) {
 
-	private static float[] AvioesDistancia(String File, float[] avioesDistancia) {
+		for (int i = 1; i < coordenadasY.length; i++) {
+			int j = i;
+			while(j < coordenadasY.length){
+				if (i == j) {
 
-		try {
-			FileReader arquivo = new FileReader(File);
-			BufferedReader lerArq = new BufferedReader(arquivo);
-			int lineCont = 1;
+					j++;
 
-			String linha = lerArq.readLine();
-			while (linha != null) {
-				if (lineCont == 1) {
-					float[] coordenadaAvioes = new float[Integer.parseInt(linha)];
 				}
+				if (!(j >= coordenadasX.length)) {
 
-				linha = lerArq.readLine();
-				lineCont++;
+					float x = coordenadasX[i] - coordenadasX[j];
+					float y = coordenadasY[i] - coordenadasY[j];
+
+					float res = (float) Math.sqrt((x * x) + (y * y));
+					if(i == 1){
+						avioesDistancia[0] = i;
+						avioesDistancia[1] = j;
+						avioesDistancia[2] = res;
+					}else if (res < avioesDistancia[2]) {
+						avioesDistancia[0] = i;
+						avioesDistancia[1] = j;
+						avioesDistancia[2] = res;
+					}
+				}
+				j++;
 			}
-
-			arquivo.close();
-		} catch (IOException e) {
-			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
 		}
-
-		avioesDistancia[0] = 1;
-		avioesDistancia[1] = 2;
-		avioesDistancia[2] = 10;
-
 		return avioesDistancia;
-
 	}
 
 	private static int NumeroAvioes(String File) {
@@ -64,29 +79,38 @@ public class MainClass {
 
 			arquivo.close();
 		} catch (IOException e) {
-			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+			System.err.printf("Erro na abertura do arquivo: %s.\n",
+					e.getMessage());
 		}
 		return 0;
 	}
 
-	private static float[] PopularArray(String File, float[] coordenadas){
+	private static void PopularArray(String File, float[] coordenadasX,
+			float[] coordenadasY) {
 		try {
 			FileReader arquivo = new FileReader(File);
 			BufferedReader lerArq = new BufferedReader(arquivo);
 
-			String linha = lerArq.readLine();
-			linha = lerArq.readLine();
-			
-			int contCoordenadas = 0;
-			while (linha != null) {
+			String line = lerArq.readLine();
+			line = lerArq.readLine();
+			int contSplit = 1;
+
+			while (line != null) {
+				String[] splitT = line.split(" ");
+				coordenadasX[contSplit] = Float.parseFloat(splitT[0]);
+				coordenadasY[contSplit] = Float.parseFloat(splitT[1]);
+
+				contSplit++;
+				line = lerArq.readLine();
 			}
-			linha = lerArq.readLine();
 
 			arquivo.close();
 		} catch (IOException e) {
-			System.err.printf("Erro na abertura do arquivo: %s.\n", e.getMessage());
+			System.err.printf("Erro na abertura do arquivo: %s.\n",
+					e.getMessage());
 		}
-		return coordenadas;
+		// return coordenadas;
 	}
-	
+
+		
 }
